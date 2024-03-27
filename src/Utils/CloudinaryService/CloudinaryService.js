dd64a290059a790c06bf594d11f0f22ec4d3f887
dev-SazzadHossain <sazzadssd512@gmail.com>
2024-03-27 14:05:29 +0600

    file_upload_method_added

---
+++ src/Utils/CloudinaryService/CloudinaryService.js
@@ -0,0 +1,25 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.COLUDINAEY_API_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET_KEY,
+});
+
+const cloudinaryService = async (localPath) => {
+  try {
+    if (localPath) {
+      const uploadResponse = await cloudinary.uploader.upload(localPath, {
+        resource_type: "auto",
+      });
+      fs.unlinkSync(localPath);
+      return uploadResponse;
+    }
+  } catch (error) {
+    fs.unlinkSync(localPath);
+    console.log(`File Upload Error Cloudinary---${error?.message}`);
+  }
+};
+
+export default cloudinaryService;
